1294a9426657271e73916b935948f417665cd40b
Apache3 <tj.thomas.jeantet@gmail.com>
2018-04-29 15:17:57 +0200

    changed serial debug

--- Inc/parameters.h
+++ Inc/parameters.h
@@ -17,10 +17,11 @@
 
 #define HZ 200
 #define DT (1.0/HZ)
-#define AUTO_STATUS_HZ 10 // must be a divider a HZ or 0 to disable
+#define AUTO_STATUS_HZ 2 // must be a divider a HZ or 0 to disable
+#define AUTO_STATUS_DT (int)1000.0*(1.0/AUTO_STATUS_HZ)
 
-#define SPD_MAX 500 //mm/s 1000
-#define ACC_MAX 750 //mm/s2
+#define SPD_MAX 1000 //mm/s 1000
+#define ACC_MAX 1500 //mm/s2
 #define RATIO_ROT_SPD_MAX 0.6
 #define K_DISTANCE_REDUCTION 20 // réduction de la vitesse linéaire quand on tourne
 #define EMERGENCY_WAIT_TIME 30 // seconds
@@ -29,7 +30,7 @@
 #define BLOCK_TIME 5000 // ms - time between each block check
 #define BLOCK_MIN_DIST 5 // mm - distance to move to consider we moved
 
-#define ENC_RESOLUTION 2048 //resolution du codeur
+#define ENC_RESOLUTION 4096 //resolution du codeur
 
 // 31,24 and 31,38 initial
 //31,05 and 31,20
@@ -37,14 +38,14 @@
 #define ENC_RIGHT_RADIUS 26.15 //REGLE PAR TEST - rayon de la roue codeuse (31.24)
 #define ENTRAXE_ENC 215.5 //(201.35)REGLE PAR TES - Distance entre chaque roue codeuse en mm (202.37), base 200,8
 
-#define ERROR_ANGLE 0.030 //erreur en angle(radians) maximale pour considérer l'objectif comme atteint
-#define ERROR_POS 5 // erreur en position (mm)  maximale pour considérer l'objectif comme atteint
+#define ERROR_ANGLE 0.010 //erreur en angle(radians) maximale pour considérer l'objectif comme atteint
+#define ERROR_POS 3 // erreur en position (mm)  maximale pour considérer l'objectif comme atteint
 #define SPD_TO_STOP 10
 
 #define CONE_ALIGNEMENT (M_PI/2.0) 
 
 #define PID_P 0.32 //0.25
-#define PID_I 10.0 //130
+#define PID_I 00.0 //130
 #define PID_D 0 // 0.5
 #define PID_BIAS 130
 
@@ -63,15 +64,12 @@
 #define RIGHT_BIAS 95
 
 #define PID_I_RATIO (1/1000.0)
-#define PID_D_RATIO (1.0)
+#define PID_D_RATIO (1/1000.0)
 
 #define TIME_BETWEEN_ORDERS 0 // s
-#define KEEP_LAST_GOAL 1
+#define KEEP_LAST_GOAL 0
 
-
-//#define SERIAL_DEBUG
-
-#define CAN_ADDR 15
+#define SERIAL_DEBUG
 
 #define digitalPinToInterrupt(p)  ((p) == 2 ? 0 : ((p) == 3 ? 1 : -1))
 
