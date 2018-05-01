051990452d68fe58c972612620877e2228c46d90
Apache3 <tj.thomas.jeantet@gmail.com>
2018-05-01 20:22:46 +0200

    added min function

--- Inc/local_math.h
+++ Inc/local_math.h
@@ -10,6 +10,10 @@
 #define MAX(a,b) a > b ? a : b
 #define sign(x) ((x)>=0?1:-1)
 #define ABS(x)   ((x) > 0 ? (x) : -(x))
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
 
 
 float moduloTwoPI(float angle);
