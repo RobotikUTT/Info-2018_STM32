97b098242ea6ee57b338aa04f9886df8fb79c714
Apache3 <tj.thomas.jeantet@gmail.com>
2018-06-06 10:52:42 +0200

    simplification of modulo function

--- Src/local_math.c
+++ Src/local_math.c
@@ -8,20 +8,16 @@
 #include <math.h>
 
 float moduloTwoPI(float angle){
-	if(angle >= 0)
 		while(angle > M_PI)
 			angle -= 2.0*M_PI;
-	else
 		while(angle <= -M_PI)
 			angle += 2.0*M_PI;
 	return angle;
 }
 
 float moduloPI(float angle){
-	if(angle >= 0)
 		while(angle > M_PI/2)
 			angle -= M_PI;
-	else
 		while(angle <= -M_PI/2)
 			angle += M_PI;
 	return angle;
